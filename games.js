22b9b8051a3a878520412b46194d579b5813ea1b
rortiz93 <ortizryan76@yahoo.com>
2023-10-11 21:21:40 -0400

    Initial commit for Rock Paper Scissors
    This is the initial commit for the rock paper scissors console
    based game. The game loops 5 times and prompts the user for an entry
    then simulating a game of rock paper scissors and keeping score.

---
+++ games.js
@@ -0,0 +1,47 @@
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    computer = options[Math.floor(Math.random() * 3)]
+    return computer;
+}
+
+function playRound(playerChoice, computerChoice){
+    playerChoice = playerChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (playerChoice == computerChoice){
+        result = "It's a tie!"
+    }else if(playerChoice == "rock" & computerChoice == "scissors"){
+        result = "You win! Rock beats scissors"
+    }else if(computerChoice == "rock" & playerChoice == "scissors"){
+        result = "You lose! Rock beats scissors"
+    }else if(playerChoice == "scissors" & computerChoice == "paper"){
+        result = "You win! Scissors beats paper"
+    }else if(computerChoice == "scissors" & playerChoice == "paper"){
+        result = "You lose! Scissors beats paper"
+    }else if(playerChoice == "paper" & computerChoice == "rock"){
+        result = "You win! Paper beats rock"
+    }else if(computerChoice == "paper" & playerChoice == "rock"){
+        result = "You lose! Paper beats rock"
+    }
+    return result;
+}
+
+function game(){
+    let i = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+    while (i <= 4){
+        let playerChoice = prompt("Please enter Rock, Paper, or Scissors");
+        let computerChoice = getComputerChoice();
+        const result = playRound(playerChoice, computerChoice);
+        if (result.includes("You win")){
+            playerScore++;
+        }else if (result.includes("You lose")) {
+            computerScore++;
+        }
+        console.log(result);
+        console.log(`Player score: ${playerScore} vs Computer Score: ${computerScore}`);
+        i++;
+    }
+}
+
+game();
