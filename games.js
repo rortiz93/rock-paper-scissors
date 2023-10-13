2b53167a9444efa0898032b3cf57f2796f91e75d
rortiz93 <ortizryan76@yahoo.com>
2023-10-13 15:45:07 -0400

    Add UI Functionality for RPS games
    This commit adds functionality to show the RPS game on the UI,
    including buttons for the player to click, a score, and a winner
    message.

--- games.js
+++ games.js
@@ -44,4 +44,58 @@ function game(){
     }
 }
 
-game();
+let playerScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
+
+let btns = document.querySelector('#button-wrapper');
+let results = document.querySelector('#results');
+let score = document.createElement('h2');
+let scoreSpan = document.createElement('span');
+let playerScoreText = document.createElement('p');
+let cpuScoreText = document.createElement('p');
+
+score.textContent = "Score";
+results.appendChild(score);
+
+playerScoreText.textContent = `Player Score: ${playerScore}`;
+scoreSpan.append(playerScoreText);
+
+cpuScoreText.textContent = `Computer Score: ${computerScore}`;
+scoreSpan.append(cpuScoreText);
+
+results.appendChild(scoreSpan);
+
+
+btns.addEventListener('click',(event) => {
+    let playerChoice = event.target.textContent;
+    let computerChoice = getComputerChoice();
+        const result = playRound(playerChoice, computerChoice);
+        gamesPlayed++;
+        if (result.includes("You win")){
+            playerScore++;
+        }else if (result.includes("You lose")) {
+            computerScore++;
+        }
+        playerScoreText.textContent = `Player Score: ${playerScore}`;
+        cpuScoreText.textContent = `Computer Score: ${computerScore}`;
+
+        if (playerScore == 5){
+            let winner = document.createElement('h3');
+            winner.textContent = 'PLAYER WINS';
+            results.appendChild(winner);
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+
+        }else if (computerScore == 5){
+            let winner = document.createElement('h3');
+            winner.textContent = 'COMPUTER WINS';
+            results.appendChild(winner);
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+        }
+        console.log(result);
+        console.log(`Player score: ${playerScore} vs Computer Score: ${computerScore}`);
+});
